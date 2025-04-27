25ade9d0abbae0e0ddf977ad6e4cd1463744033f
Kian <Kian.berg333@gmail.com>
2025-04-26 23:02:50 -0600

    Introduce framework for buttons and input

---
+++ javascript.js
@@ -0,0 +1,11 @@
+const input1 = document.querySelector(".input1");
+const input2 = document.querySelector(".input2");
+const input3 = document.querySelector(".input3");
+
+const del1 = document.querySelector(".del1");
+const del2 = document.querySelector(".del2");
+const del3 = document.querySelector(".del3");
+
+const resultsBtn = document.querySelector(".results-btn");
+const resultsList = document.querySelector(".results-list");
+
